e76d7eb332dc2a85b821874175dfac724d44f15a
Eva <Ohmu@Evas-MacBook-Pro.local>
2015-06-19 15:41:06 -0400

    removing build stuff for gh-pages

--- README.md
+++ README.md
@@ -1,11 +1,1 @@
-## Installation
-
-1. Clone this repo.
-
-2. cd into the directory and run `npm install`
-
-3. Run `gulp` and it will pull in all dependencies and compile.
-
-4. Run `gulp watch` to recompile as you make edits
-
-The public/ directory will contain files to host.
+gh-pages
--- gulpfile.js
+++
@@ -1,71 +0,0 @@
-var gulp = require('gulp'),     
-    sass = require('gulp-ruby-sass') ,
-    notify = require("gulp-notify") ,
-    bower = require('gulp-bower'),
-    uglify = require('gulp-uglify'),
-    concat = require('gulp-concat'),
-    notify = require('gulp-notify'),
-    rename = require('gulp-rename');
-
-var config = {
-     sassPath: './resources/sass',
-     bowerDir: './bower_components' 
-}
-
-gulp.task('bower', function() { 
-    return bower()
-         .pipe(gulp.dest(config.bowerDir)) 
-});
-
-gulp.task('icons', function() { 
-    return gulp.src(config.bowerDir + '/fontawesome/fonts/**.*') 
-        .pipe(gulp.dest('./public/fonts')); 
-});
-
-gulp.task('images', function() { 
-    return gulp.src('./resources/img/**.*') 
-        .pipe(gulp.dest('./public/img')); 
-});
-
-// uglify task
-gulp.task('js', function() {
-	gulp.src([
-		config.bowerDir + '/jquery/dist/jquery.js',
-		config.bowerDir + '/bootstrap/dist/js/bootstrap.js',
-		])
-		.pipe(concat('app.js'))
-		.pipe(gulp.dest('./public/js'))
-		.pipe(uglify())
-		.pipe(rename('app.min.js'))
-		.pipe(uglify())
-        .pipe(gulp.dest('./public/js'))
-});
-
-gulp.task('sass', function() { 
-    return sass(config.sassPath + '/style.scss', 
-	{
-             style: 'compressed',
-             loadPath: [
-                 './resources/sass',
-                 config.bowerDir + '/bootstrap-sass-official/assets/stylesheets',
-                 config.bowerDir + '/fontawesome/scss',
-             ]
-         }) 
-         .on("error", notify.onError(function (error) {
-                 return "Error: " + error.message;
-             })) 
-         .pipe(gulp.dest('./public/css')); 
-});
-
-// Rerun the task when a file changes
- gulp.task('watch', function() {
-     gulp.watch(config.sassPath + '/**/*.scss', ['images', 'sass']); 
-
-	gulp.watch('./resources/js/**/*.js', function() {
-			gulp.run('js');
-		});
-	});
-
-gulp.task('default', ['bower', 'icons', 'js', 'images', 'sass']);
-
-
